7f9994640787af73a93a0b47c45b9ac7f0b80c71
Heavey <pheave200@cable.comcast.com>
2020-04-15 16:49:30 -0400

    1st Day of Asignment 5

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+      if(pilotName.value==="" || copilotName.value==="" || fuelLevel.value==="" || cargoMass.value==="") { 
+         alert("All Fields Are Required!");  
+      }
+      if(fuelLevel.value<10000 && cargoMass.value>10000) {
+         let div = document.getElementById("faultyItems")
+         div.innerHTML = `
+            
+         `;
+      }
+   });
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+         let div = document.getElementById("missionTarget");
+         div.innerHTML = `
+            <h2>Mission Destination</h2>
+            <ul>
+               <li>Name: ${json[2].name}</li>
+               <li>Diameter: ${json[2].diameter}</li>
+               <li>Star: ${json[2].star}</li>
+               <li>Distance From Earth:${json[2].distance}</li>
+               <li>Number of Moons: ${json[2].moons}</li>
+            </ul>
+            <img src="${json[2].image}">
+         `;
+      });
+   });
+
+
+})
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
